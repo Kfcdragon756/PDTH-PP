4154899618d2578f18add83f11446c1e18139f22
Kfcdragon756 <2504809701@qq.com>
2025-06-01 14:42:59 +0800

    Buffed team bots' health, reaction timer under Overkill/Overkill 145+. Increased Team bots' speed.

--- difficulties/charactertweakdata.lua
+++ difficulties/charactertweakdata.lua
@@ -47,10 +47,10 @@ module:hook(CharacterTweakData, "_set_hard", function(self)
 	self:_multiply_weapon_delay(self.presets.weapon.fbi, 0.3)
 	self:_multiply_weapon_delay(self.presets.weapon.sniper, 1.5)
 	self:_multiply_weapon_delay(self.presets.weapon.gang_member, 0.5)
-	self.presets.gang_member_damage.MIN_DAMAGE_INTERVAL = 0.3
+	self.presets.gang_member_damage.MIN_DAMAGE_INTERVAL = 0.5
 	self.presets.gang_member_damage.REGENERATE_TIME = 2
 	self.presets.gang_member_damage.REGENERATE_TIME_AWAY = 0.35
-	self.presets.gang_member_damage.HEALTH_INIT = 50
+	self.presets.gang_member_damage.HEALTH_INIT = 70
 	self.presets.gang_member_damage.BLEED_OUT_HEALTH_INIT = 100
 	self.tank.damage.visor_health = 65
 	self.tank.damage.visor_explosion_health = 65 -- tweak me
@@ -65,10 +65,10 @@ module:hook(CharacterTweakData, "_set_overkill", function(self)
 	self:_multiply_weapon_delay(self.presets.weapon.fbi, 0.25)
 	self:_multiply_weapon_delay(self.presets.weapon.sniper, 0.5)
 	self:_multiply_weapon_delay(self.presets.weapon.gang_member, 0.75)
-	self.presets.gang_member_damage.MIN_DAMAGE_INTERVAL = 0.3
+	self.presets.gang_member_damage.MIN_DAMAGE_INTERVAL = 0.5
 	self.presets.gang_member_damage.REGENERATE_TIME = 3
 	self.presets.gang_member_damage.REGENERATE_TIME_AWAY = 0.35
-	self.presets.gang_member_damage.HEALTH_INIT = 50
+	self.presets.gang_member_damage.HEALTH_INIT = 65
 	self.presets.gang_member_damage.BLEED_OUT_HEALTH_INIT = 100
 	self.tank.damage.visor_health = 70
 	self.tank.damage.visor_explosion_health = 70 -- tweak me
@@ -83,11 +83,11 @@ module:hook(CharacterTweakData, "_set_overkill_145", function(self)
 	self:_multiply_weapon_delay(self.presets.weapon.expert, 0.15)
 	self:_multiply_weapon_delay(self.presets.weapon.fbi, 0.15)
 	self:_multiply_weapon_delay(self.presets.weapon.sniper, 0.5)
-	self:_multiply_weapon_delay(self.presets.weapon.gang_member, 1.5)
-	self.presets.gang_member_damage.MIN_DAMAGE_INTERVAL = 0.3
+	self:_multiply_weapon_delay(self.presets.weapon.gang_member, 0.75)
+	self.presets.gang_member_damage.MIN_DAMAGE_INTERVAL = 0.4
 	self.presets.gang_member_damage.REGENERATE_TIME = 3
 	self.presets.gang_member_damage.REGENERATE_TIME_AWAY = 0.35
-	self.presets.gang_member_damage.HEALTH_INIT = 50
+	self.presets.gang_member_damage.HEALTH_INIT = 60
 	self.presets.gang_member_damage.BLEED_OUT_HEALTH_INIT = 100
 	self.tank.damage.visor_health = 75
 	self.tank.damage.visor_explosion_health = 75 -- tweak me
