0b6e2c09c39274b2abb08879b1a0e046c65069b3
Kfcdragon756 <2504809701@qq.com>
2025-07-11 10:06:16 +0800

    Tweaks on bots.

--- difficulties/charactertweakdata.lua
+++ difficulties/charactertweakdata.lua
@@ -30,9 +30,9 @@ module:hook(CharacterTweakData, "_set_normal", function(self)
 	self:_multiply_weapon_delay(self.presets.weapon.sniper, 2)
 	self:_multiply_weapon_delay(self.presets.weapon.gang_member, 0.25)
 	self.presets.gang_member_damage.MIN_DAMAGE_INTERVAL = 0.4
-	self.presets.gang_member_damage.REGENERATE_TIME = 2
-	self.presets.gang_member_damage.REGENERATE_TIME_AWAY = 0.35
-	self.presets.gang_member_damage.HEALTH_INIT = 70
+	self.presets.gang_member_damage.REGENERATE_TIME = 2.5
+	self.presets.gang_member_damage.REGENERATE_TIME_AWAY = 0.4
+	self.presets.gang_member_damage.HEALTH_INIT = 65
 	self.presets.gang_member_damage.BLEED_OUT_HEALTH_INIT = 35
 	self.tank.damage.visor_health = 50
 	self.tank.damage.visor_explosion_health = 50 -- tweak me
@@ -48,9 +48,9 @@ module:hook(CharacterTweakData, "_set_hard", function(self)
 	self:_multiply_weapon_delay(self.presets.weapon.sniper, 1.5)
 	self:_multiply_weapon_delay(self.presets.weapon.gang_member, 0.5)
 	self.presets.gang_member_damage.MIN_DAMAGE_INTERVAL = 0.35
-	self.presets.gang_member_damage.REGENERATE_TIME = 2
-	self.presets.gang_member_damage.REGENERATE_TIME_AWAY = 0.35
-	self.presets.gang_member_damage.HEALTH_INIT = 70
+	self.presets.gang_member_damage.REGENERATE_TIME = 3
+	self.presets.gang_member_damage.REGENERATE_TIME_AWAY = 0.45
+	self.presets.gang_member_damage.HEALTH_INIT = 60
 	self.presets.gang_member_damage.BLEED_OUT_HEALTH_INIT = 35
 	self.tank.damage.visor_health = 65
 	self.tank.damage.visor_explosion_health = 65 -- tweak me
@@ -66,9 +66,9 @@ module:hook(CharacterTweakData, "_set_overkill", function(self)
 	self:_multiply_weapon_delay(self.presets.weapon.sniper, 0.5)
 	self:_multiply_weapon_delay(self.presets.weapon.gang_member, 0.75)
 	self.presets.gang_member_damage.MIN_DAMAGE_INTERVAL = 0.3
-	self.presets.gang_member_damage.REGENERATE_TIME = 3
-	self.presets.gang_member_damage.REGENERATE_TIME_AWAY = 0.35
-	self.presets.gang_member_damage.HEALTH_INIT = 65
+	self.presets.gang_member_damage.REGENERATE_TIME = 3.5
+	self.presets.gang_member_damage.REGENERATE_TIME_AWAY = 0.5
+	self.presets.gang_member_damage.HEALTH_INIT = 55
 	self.presets.gang_member_damage.BLEED_OUT_HEALTH_INIT = 32.5
 	self.tank.damage.visor_health = 70
 	self.tank.damage.visor_explosion_health = 70 -- tweak me
@@ -85,9 +85,9 @@ module:hook(CharacterTweakData, "_set_overkill_145", function(self)
 	self:_multiply_weapon_delay(self.presets.weapon.sniper, 0.5)
 	self:_multiply_weapon_delay(self.presets.weapon.gang_member, 0.75)
 	self.presets.gang_member_damage.MIN_DAMAGE_INTERVAL = 0.25
-	self.presets.gang_member_damage.REGENERATE_TIME = 3
-	self.presets.gang_member_damage.REGENERATE_TIME_AWAY = 0.35
-	self.presets.gang_member_damage.HEALTH_INIT = 60
+	self.presets.gang_member_damage.REGENERATE_TIME = 4
+	self.presets.gang_member_damage.REGENERATE_TIME_AWAY = 0.55
+	self.presets.gang_member_damage.HEALTH_INIT = 50
 	self.presets.gang_member_damage.BLEED_OUT_HEALTH_INIT = 30
 	self.tank.damage.visor_health = 75
 	self.tank.damage.visor_explosion_health = 75 -- tweak me
