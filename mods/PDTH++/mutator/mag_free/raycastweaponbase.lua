bbcdd61f1b6472dbc657b3bb4b4323fd7b93d8e2
Kfcdragon756 <2504809701@qq.com>
2025-11-07 18:00:45 +0800

    Extra start out ammo update.
    -Extra start out ammo now provides instant reload after interacting with an ammo bag.
    
    -Another attempt to prevent non-sentry-takers to take up the sentry gun.

--- mods/PDTH++/mutator/mag_free/raycastweaponbase.lua
+++ mods/PDTH++/mutator/mag_free/raycastweaponbase.lua
@@ -3,7 +3,7 @@ local RaycastWeaponBase = module:hook_class("RaycastWeaponBase")
 
 module:hook(RaycastWeaponBase, "on_reload", function(self)
 	if self._setup.expend_ammo then
-		if self._ammo_remaining_in_clip > 0 then
+		if self._ammo_remaining_in_clip > 0 and self._ammo_remaining_in_clip ~= self._ammo_max_per_clip then
 			self._ammo_total = self._ammo_total - self._ammo_remaining_in_clip
 		end
 
