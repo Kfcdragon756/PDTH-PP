e6458558a163bda0dc3ed69d4d4f8687968188b8
Kfcdragon756 <2504809701@qq.com>
2026-07-25 12:10:30 +0800

    Forgot this. this won't cause any issue anyway.

--- mods/PDTH++/mutator/combine_assault/groupaitweakdata.lua
+++ mods/PDTH++/mutator/combine_assault/groupaitweakdata.lua
@@ -1,27 +1,6 @@
 local module = ... or D:module("PDTH++")
 local GroupAITweakData = module:hook_class("GroupAITweakData")
 
-local function _remove_blockade_murkies(self)
-	-- Aggressive safety: street blockade uses very map-specific defend/frontal/flank points.
-	-- Murkies tend to expose bad/unsupported pathing there on some maps, so keep them
-	-- in regular assault pools but do not let them become blockade specialists.
-	if self.street and self.street.blockade and self.street.blockade.units then
-		local blockade = self.street.blockade.units
-		if blockade.defend then
-			blockade.defend.murky = nil
-			blockade.defend.fbi = blockade.defend.fbi or { 0.35, 0.5, 0.65 }
-		end
-		if blockade.frontal then
-			blockade.frontal.murky = nil
-			blockade.frontal.fbi = blockade.frontal.fbi or { 0.35, 0.6, 0.8 }
-		end
-		if blockade.flank then
-			blockade.flank.murky = nil
-			blockade.flank.fbi = blockade.flank.fbi or { 1, 1, 1 }
-		end
-	end
-end
-
 module:post_hook(GroupAITweakData, "_set_overkill", function(self)
 	self.besiege.assault.units.swat = { 0.25, 0.35, 0.5 }
 	self.besiege.assault.units.swat_kevlar = { 0.25, 0.5, 0.6 }
@@ -32,10 +11,10 @@ module:post_hook(GroupAITweakData, "_set_overkill", function(self)
 	self.street.assault.units.swat_kevlar = { 0.4, 0.5, 0.5 }
 	self.street.assault.units.murky = { 0.7, 0.7, 0.7 }
 
-	self.street.blockade.units.frontal.swat = { 0.05, 0.05, 0.1 }
+	self.street.blockade.units.frontal.swat = { 0.05, 0.05, 0.1 }	
 	self.street.blockade.units.frontal.swat_kevlar = { 0.1, 0.1, 0.2 }
-
-	_remove_blockade_murkies(self)
+	self.street.blockade.units.frontal.murky = { 0.2, 0.6, 0.8 }
+	self.street.blockade.units.flank.murky = { 1, 1, 1 }
 end, false)
 
 module:post_hook(GroupAITweakData, "_set_overkill_145", function(self)
@@ -47,10 +26,11 @@ module:post_hook(GroupAITweakData, "_set_overkill_145", function(self)
 	self.street.assault.units.swat = { 0.5, 0.5, 0.5 }
 	self.street.assault.units.swat_kevlar = { 0.5, 0.5, 0.5 }
 	self.street.assault.units.murky = { 1, 1, 1 }
+	self.street.blockade.units.defend.murky = { 1, 1, 1 }
 	self.street.blockade.units.defend.swat = { 0.05, 0.05, 0.1 }
 	self.street.blockade.units.defend.swat_kevlar = { 0.4, 0.5, 0.5 }
-	self.street.blockade.units.frontal.swat = { 0.2, 0.2, 0.4 }
+	self.street.blockade.units.frontal.swat = { 0.2, 0.2, 0.4 }	
 	self.street.blockade.units.frontal.swat_kevlar = { 0.1, 0.1, 0.1 }
-
-	_remove_blockade_murkies(self)
+	self.street.blockade.units.frontal.murky = { 0.4, 1, 1 }
+	self.street.blockade.units.flank.murky = { 1, 1, 1 }
 end, false)
